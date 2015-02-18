829e6fb4ca788c03971bd964c5fc38d2b2fcb3c1
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-18 17:26:33 -0500

    Add Basic Stubs for Impl

---
+++ ML-Algos/logistic-regression/LogisticRegression.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 18, 2015
+
+@author: santhosh
+'''
+class LogisticRegression:
+ def __init__(self):
+     pass
+ def fit(self, X, y):
+     pass
+ 
+ def predict(self, X):
+     pass
---
+++ ML-Algos/naive-bayes/NaiveBayes.py
@@ -0,0 +1,14 @@
+'''
+Created on Feb 18, 2015
+
+@author: santhosh
+'''
+
+class NaiveBayesClassifier:
+ def __init__(self):
+     pass
+ def fit(self, X, y):
+     pass
+ 
+ def predict(self, X):
+     pass
