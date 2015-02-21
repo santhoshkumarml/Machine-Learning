54306dfebabbd7ce26493de63de86c73ad725e3b
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-20 20:13:37 -0500

    Add DataReader

--- ML-Algos/naive-bayes/NaiveBayes.py
+++ ML-Algos/naive-bayes/NaiveBayes.py
@@ -4,11 +4,22 @@ Created on Feb 18, 2015
 @author: santhosh
 '''
 
+import numpy
+
+
 class NaiveBayesClassifier:
  def __init__(self):
+     self.cpt = []
      pass
+ 
+ def addOneSmoothing(self):
+     pass
+ 
  def fit(self, X, y):
-     pass
+     k = set(y)
+     d = X.shape[0]
+     
+     self.cpt = numpy.zeros(shape = (),dtype = float32)
  
  def predict(self, X):
      pass
