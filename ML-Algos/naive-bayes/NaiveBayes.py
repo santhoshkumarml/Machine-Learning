f30c7bb8ebc9bf11d6cfde978483f8a26b33f9a9
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-21 00:41:53 -0500

    Changes

--- ML-Algos/naive-bayes/NaiveBayes.py
+++ ML-Algos/naive-bayes/NaiveBayes.py
@@ -6,22 +6,38 @@ Created on Feb 18, 2015
 
 import numpy
 
-
 class NaiveBayesClassifier:
  def __init__(self):
-     self.cpt = []
+     self.jpd = []
+     self.fp = []
+     self.tp = []
      pass
  
- def addOneSmoothing(self):
-     pass
  
  def fit(self, X, y):
-     self.cpt = numpy.zeros(shape = (),dtype = float32)
+     train_samples,train_features = X.shape
+     no_of_classes = len(set(y))
      
-     for row in X:
-         for i in range(d):
-                 pass
+     total_words = sum([sum(X[i]) for i in range(train_samples)])
      
- 
+     self.cpt = numpy.zeros(shape = (train_features, no_of_classes),dtype = float32)
+     self.tp = numpy.zeros(no_of_classes)
+     self.fp = numpy.zeros(train_features)
+     
+     for i in range(train_samples):
+         classified_class = y[i]
+         self.tp[classified_class] += 1
+         for j in range(train_features):
+             #total_count_of_jth_feature = sum(X[:,j])
+             self.cpt[j][classified_class] += X[i][j]
+             self.fp[j]+=1
+            
+     for i in range(train_features):
+         total_count_of_ith_feature = sum(self.cpt[i])
+         self.fp[i] /= total_words
+         for j in range(no_of_classes):
+             self.cpt[i][j] += 1
+             self.cpt[i][j] /= (total_count_of_ith_feature + total_words) 
+
  def predict(self, X):
      pass
--- ML-Algos/tryout/tryout.py
+++ ML-Algos/tryout/tryout.py
@@ -4,8 +4,40 @@ Created on Feb 20, 2015
 @author: santhosh
 '''
 
+
+import naive-bayes.NaiveBayes.NaiveBayesClassifier as nbclassifier
 import numpy
 
-array = numpy.arange(0,1000,1)
-array = numpy.reshape(array, (200,5))
-print array[:,0]
+train_data = '../train.data'
+train_target = '../train.label'
+
+data = []
+y = []
+
+with open(train_data) as f:
+    lines = f.readlines()
+    for line in lines:
+        doc_id, word_id, count = line.split()
+        doc_id = int(doc_id)
+        word_id = int(word_id)
+        count = int(count)
+        data.append([doc_id, word_id, count])
+
+data = numpy.array(data)
+
+with open(train_target) as f:
+    lines = f.readlines()
+    for line in lines:
+        classified_as = line.strip()
+        y.append(int(classified_as))
+
+max_doc_id = max(data[:,0])
+max_word_id = max(data[:,1])
+
+X = numpy.array(shape = (max_doc_id, max_word_id), dtype = int)
+for doc_id, word_id, count in data:
+    X[doc_id-1][word_id-1] = count
+y = numpy.array(y)
+
+nb = nbclassifier()
+nb.fit()
