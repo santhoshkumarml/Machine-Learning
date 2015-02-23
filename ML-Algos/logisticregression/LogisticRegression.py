fb8fb91af40fecdaf4986d98f04166f35a9983f1
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-22 22:46:11 -0500

    Changes

--- ML-Algos/logisticregression/LogisticRegression.py
+++ ML-Algos/logisticregression/LogisticRegression.py
@@ -6,6 +6,7 @@ Created on Feb 18, 2015
 
 import math
 import numpy
+import sys
 
 class LogisticRegression:
     def __init__(self, step_size):
@@ -22,9 +23,17 @@ class LogisticRegression:
         calc = self.calculateExponentOfVectors(paramVector, X)
         return calc/(1+calc)
     
-    def calculateHypothesis(self, paramVector, X):
+    def getChange_X(self, X):
         changed_X = numpy.insert(X, 0, 1)
+        return changed_X
+    
+    def getChange_X_AndTranspose(self, X):
+        changed_X = self.getChange_X(X)
         changed_X_t = numpy.transpose(changed_X)
+        return changed_X_t
+    
+    def calculateHypothesis(self, paramVector, X):
+        changed_X_t = self.getChange_X_AndTranspose(X)    
         return self.sigmoid(self.paramVector, changed_X_t)
     
     def determine_step_factor(self, j, paramVector, X, y):
@@ -48,13 +57,13 @@ class LogisticRegression:
         #no_of_classes = len(set(y))
      
         # estimate parameters
-        self.paramVector = numpy.zeros(shape = (1, train_features+1))
+        self.paramVector = numpy.zeros(train_features+1)
         log_likelihood = self.calculateLogLikeliHood(train_samples, X, y, self.paramVector)
         
         #iterative gradient ascend and estimate parameters
         while True:
-            paramVector = numpy.zeros(shape = (1, train_features+1))
-            for j in range(len(self.paramVector)):
+            paramVector = numpy.zeros(train_features+1)
+            for j in range(train_features+1):
                 step_factor = 0
                 for i in range(train_samples):
                     step_factor += self.determine_step_factor(j, self.paramVector,\
@@ -67,6 +76,9 @@ class LogisticRegression:
             else:
                 log_likelihood = new_log_likelihood
                 self.paramVector = paramVector
+                
+        for i in range(train_features+1):
+            print self.paramVector[i]
     
     def predict(self, X):
         predicted_value =  self.calculateHypothesis(self.paramVector, X)
