dd44d474904d5c5a852508254dfe726ef012c4ed
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-03-22 04:14:09 -0400

    Make the implementation of ada boost (Decision Stump need to be implemented) come to a logical stop

--- Assigment2/ada_boost/AdaBoost.py
+++ Assigment2/ada_boost/AdaBoost.py
@@ -1,5 +1,6 @@
 __author__ = 'santhosh'
 import numpy
+import math
 
 
 class DecisionStump(object):
@@ -24,6 +25,19 @@ class AdaBoostClassifier(object):
         weights = numpy.array([(1.0/n_samples) for i in range(n_samples)])
         for i in range(self.iterations):
             self.weakClassifiers[i].fit(train_data, train_result, weights)
+            # determine alpha_t
+            training_error_results = [1 if train_result[i] !=\
+                                          self.weakClassifiers[i].predict(train_data[i]) else 0]
+            training_error = float(sum(training_error_results))/n_samples
+            inner_calc_for_alpha = (1-training_error)/training_error
+            self.alphas[i] = 0.5*math.log(inner_calc_for_alpha, math.exp(1))
+            #redetermine weights for next round
+            new_weights = [weights[i]*\
+                           math.exp(-1*self.alphas[i]*train_result[i]*\
+                                    self.weakClassifiers[i].predict(train_data[i]))\
+                           for i in range(n_samples)]
+            z_t = sum(new_weights)
+            weights = numpy.array([new_weights[i]/z_t for i in range(n_samples)])
 
     def predict(self, test_data_instance):
         final_value = 0
