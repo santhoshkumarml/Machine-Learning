fb10d1e7f87bf9bcede868d96e1f1bae38d31707
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-03-22 03:52:19 -0400

    Minor Modifications

--- Assigment2/ada_boost/AdaBoost.py
+++ Assigment2/ada_boost/AdaBoost.py
@@ -3,19 +3,26 @@ import numpy
 
 
 class DecisionStump(object):
-    def __init__(self, data):
+    def __init__(self):
         pass
 
+    def fit(self, train_data, train_results, weights):
+        pass
 
 class AdaBoostClassifier(object):
-    def __init__(self, train_data, train_result):
-        n_samples,n_features = train_data.shape
-        weights = numpy.array([(1.0/n_samples) for i in range(n_samples)])
-        self.weighted_data = numpy.column_stack((train_data,weights))
-        self.results = train_result
+    def __init__(self,iterations=100):
+        self.weights = []
+        self.results = []
+        self.iterations = iterations
+        self.weakClassifiers = numpy.arr
+        self.alphas = numpy.zeros(self.iterations)
 
-    def fit(self):
-        pass
+    def fit(self, train_data, train_result):
+        n_samples,n_features = train_data.shape
+        self.weights = numpy.array([(1.0/n_samples) for i in range(n_samples)])
+        self.results = train_result
+        for i in range(self.iterations):
+            pass
 
     def predict(self, test_data):
-        print self.weighted_data.shape, self.results.shape
+        pass
