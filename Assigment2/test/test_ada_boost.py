6508fd9644cf519f5e6caaeba8b216bca2a73ea4
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-03-25 18:57:31 -0400

    Adding print statments for question 1 and question 2

--- Assigment2/test/test_ada_boost.py
+++ Assigment2/test/test_ada_boost.py
@@ -25,9 +25,6 @@ def plotError(error_per_iteration, pltFile, label):
 # test_result = train_result
 # adc = AdaBoostClassifier(4)
 # adc.fitPredictAndScore(train_data, train_result, test_data, test_result)
-#
-# import sys
-# sys.exit()
 
 # question 1
 print '--------------------------------------------------'
@@ -35,12 +32,12 @@ before = datetime.datetime.now()
 T = 100
 adc = AdaBoostClassifier(T)
 avg_train_error, avg_test_error = [0 for i in range(T)], [0 for i in range(T)]
-no_of_random_samples = 1
+no_of_random_samples = 50
 for i in range(no_of_random_samples):
     train_data, train_result, test_data, test_result = data_reader.read_data(data_split=0.9)
     print 'Random Sample ',str(i+1)
     train_error_for_iters, test_error_for_iters = adc.fitPredictAndScore(train_data, train_result, test_data, test_result, calc_error_each_iter=True)
-    print ' Train Error:', str(train_error_for_iters[-1]),' Test Error:',str(test_error_for_iters[-1])
+    print 'Train Error:', str(train_error_for_iters[-1]),' Test Error:',str(test_error_for_iters[-1])
     for j in range(len(train_error_for_iters)):
         avg_train_error[j] += train_error_for_iters[j]
         avg_test_error[j] += test_error_for_iters[j]
@@ -48,8 +45,8 @@ for i in range(no_of_random_samples):
 avg_train_error = [avg_train_error[i]/no_of_random_samples for i in range(T)]
 avg_test_error = [avg_test_error[i]/no_of_random_samples for i in range(T)]
 
-plotError(avg_train_error, 'Avg Train Error', 'Avg Train Error For '+str(no_of_random_samples)+' random splits and '+str(T)+' iterations of boosting')
-plotError(avg_test_error, 'Avg Test Error', 'Avg Test Error For '+str(no_of_random_samples)+' random splits and '+str(T)+' iterations of boosting')
+plotError(avg_train_error, 'Avg Train Error', 'Avg Train Error For '+str(no_of_random_samples)+' random splits and '+str(T)+' iterations')
+plotError(avg_test_error, 'Avg Test Error', 'Avg Test Error For '+str(no_of_random_samples)+' random splits and '+str(T)+' iterations')
 print 'Question 1 Done - Average Train and Test Error For '+str(no_of_random_samples)+' random splits and '+str(T)+' iterations of boosting'
 after = datetime.datetime.now()
 print 'Time Taken:', after-before
