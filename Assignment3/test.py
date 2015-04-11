cfe6d6c76c293a8d5a2f56f5d8cc4b7f35602f8e
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-04-10 20:51:16 -0400

    Changes

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -3,10 +3,12 @@ __author__ = 'santhosh'
 import scipy.io
 import os
 from knn import KNN
+import numpy
+
 matlab_data = scipy.io.loadmat(os.path.join(os.getcwd(),'resources/faces.mat'))
 train_data = matlab_data['traindata']
-train_result = matlab_data['trainlabels']
+train_result = numpy.array([labels[0] for labels in matlab_data['trainlabels']])
 test_data = matlab_data['testdata']
-test_result = matlab_data['testlabels']
+test_result = numpy.array([labels[0] for labels in matlab_data['testlabels']])
 knnClassifier = KNN(range(1,100), 10)
 knnClassifier.fit(train_data,train_result)
