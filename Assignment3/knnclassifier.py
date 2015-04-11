7a14999d276c28c817402638c9c3e30ea50ab203
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-04-11 00:47:26 -0400

    Changes

--- Assignment3/knnclassifier.py
+++ Assignment3/knnclassifier.py
@@ -39,7 +39,8 @@ class KNNClassifier(object):
         for i in range(n_samples):
             neighbor_distances = {j:self.cosine_distance(self.train_data[i], self.train_data[j])\
                                   for j in range(n_samples) if i != j}
-            neighbor_distances = sorted(list(neighbor_distances.keys()), key = lambda key : neighbor_distances[key])
+            neighbor_distances = sorted(list(neighbor_distances.keys()),\
+                key = lambda key : neighbor_distances[key])
             self.train_sample_pair_wise_distance_matrix[i] = neighbor_distances
         
     def findKNearestNeigbors(self, x, k = None):
@@ -51,7 +52,8 @@ class KNNClassifier(object):
             train_vector = self.train_data[i]
             distance_for_this_sample = self.cosine_distance(train_vector, x)
             neighbor_distances_dict[i] = distance_for_this_sample
-        neighbors_array = sorted(list(neighbor_distances_dict.keys()), key = lambda key: neighbor_distances_dict[key])
+        neighbors_array = sorted(list(neighbor_distances_dict.keys()),\
+            key = lambda key: neighbor_distances_dict[key])
         return neighbors_array[:k]
 
     def doNFoldCrossValidationAndMeasureK(self):
@@ -61,8 +63,19 @@ class KNNClassifier(object):
         for i in range(0, n_samples, partition_size):
             test_idx_for_each_iter.append((i,i+partition_size))
         cross_validation_error_for_each_k = dict()
+        train_error_for_each_k = []
         for k in self.possible_k_values:
-            error = 0
+            cross_validation_error = 0.0
+            train_error = 0.0
+
+            for sample_idx in range(n_samples):
+                knn_for_train = [neighbor for neighbor in self.train_sample_pair_wise_distance_matrix[sample_idx]][:k]
+                label = self.getMajorityClassLabelsForKNN(knn_for_train,\
+                                                          self.train_data[sample_idx],\
+                                                          self.train_result[sample_idx])
+                if label != self.train_result[sample_idx]:
+                    train_error += 1.0
+
             for i in range(len(test_idx_for_each_iter)):
                 start, end = test_idx_for_each_iter[i]
                 train_data_for_iter, train_result_for_iter = [],[]
@@ -70,16 +83,23 @@ class KNNClassifier(object):
                     if sample_idx < start or sample_idx >= end:
                         train_data_for_iter.append(self.train_data[sample_idx])
                         train_result_for_iter.append(self.train_result[sample_idx])
-                for test_idx in range(start,end):
-                    knn = [neighbor for neighbor in self.train_sample_pair_wise_distance_matrix[test_idx] if neighbor < start or neighbor >= end][:k]
+                for test_idx in range(start, end):
+                    knn = [neighbor for neighbor in\
+                           self.train_sample_pair_wise_distance_matrix[test_idx]\
+                           if neighbor < start or neighbor >= end][:k]
                     label = self.getMajorityClassLabelsForKNN(knn, train_data_for_iter, train_result_for_iter)
                     test_label = self.train_result[test_idx]
                     if label != test_label:
-                        error += 1
-            cross_validation_error_for_each_k[k] = error/n_samples
-        self.k = min(cross_validation_error_for_each_k.keys(), key = lambda key: cross_validation_error_for_each_k[key])
+                        cross_validation_error += 1
+            cross_validation_error_for_each_k[k] = cross_validation_error/n_samples
+            train_error /= n_samples
+            train_error_for_each_k.append(train_error)
+
+        self.k = min(cross_validation_error_for_each_k.keys(),\
+            key = lambda key: cross_validation_error_for_each_k[key])
         print 'Minimum error with k: ', self.k
-        return cross_validation_error_for_each_k.values()
+
+        return cross_validation_error_for_each_k.values(), train_error_for_each_k
 
     def getMajorityClassLabelsForKNN(self, knn, train_data, train_result):
         class_labels = dict()
@@ -110,12 +130,10 @@ class KNNClassifier(object):
         self.train_data = train_data
         self.train_result = train_result
         self.calculatePairWiseDistanceForTrainSamples()
-        cross_validation_error_for_each_k = self.doNFoldCrossValidationAndMeasureK()
-        train_error_for_each_k = []
+        cross_validation_error_for_each_k, train_error_for_each_k = self.doNFoldCrossValidationAndMeasureK()
         test_error_for_each_k = []
         for k in self.possible_k_values:
-            train_error = self.predictAndScore(train_data, train_result, k)
-            train_error_for_each_k.append(train_error)
             test_error = self.predictAndScore(test_data, test_result, k)
             test_error_for_each_k.append(test_error)
-        plotErrorForK(self.possible_k_values, cross_validation_error_for_each_k, train_error_for_each_k, test_error_for_each_k)
+        plotErrorForK(self.possible_k_values, cross_validation_error_for_each_k,\
+                      train_error_for_each_k, test_error_for_each_k)
